d613fcf20fe5742fb71bcaf050b9db6a93ab3a63
dillontkh <>
2025-02-14 19:41:48 +0800

    feat: update option images and add opening image

--- my-app/src/app/components/start_scene.js
+++ my-app/src/app/components/start_scene.js
@@ -3,6 +3,8 @@ import {
   useUtilitiesTracker,
   useCurrentScene,
 } from "../hooks/stats";
+import Image from "next/image";
+import openingImage from "../../../public/images/OPENING_IMAGE.png";
 
 const StartScene = () => {
   const playGame = useCurrentGameState((state) => state.setToPlay);
@@ -11,7 +13,7 @@ const StartScene = () => {
 
   const resetSceneId = useCurrentScene((state) => state.resetSceneId);
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-8">
       <div className="flex flex-col text-lg leading-relaxed max-w-3xl mx-auto text-center bg-white rounded-lg shadow-lg gap-2 p-2">
         <p>
           In this immersive simulation, you'll navigate a day in a resource-constrained world, making critical decisions about water and electricity usage that affects your health.
@@ -23,8 +25,13 @@ const StartScene = () => {
           Will you prioritize personal comfort or resource conservation?
         </p>
       </div>
+      <Image
+        src={openingImage}
+        alt="Opening Image"
+        className="w-full max-w-3xl h-auto rounded-lg shadow-lg"
+      />
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-8 px-16 rounded mt-8 text-4xl"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-8 px-16 rounded text-4xl"
         onClick={() => {
           playGame();
           resetUtilities();
